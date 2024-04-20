c8120c75dccfd8f7cb30a6ee0f9b25eb5e70dda7
johnmart19 <johnivan19999@gmail.com>
2024-04-20 04:13:22 +0300

    munch: Camera: Enable newer HIDL overrideFormat (Another Implementation)
    Introduced in Evolution-X/hardware_interfaces@c7ca35486ad84c684b7909bc483241a16792e4d4
    
    Previously was enabled by default

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,6 +70,8 @@ MALLOC_SVELTE_FOR_LIBC32 := true
 
 # Camera - HIDL overrideFormat
 TARGET_CAMERA_USES_NEWER_HIDL_OVERRIDE_FORMAT = true
+# Camera - HIDL overrideFormat - Alternative Version
+TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED = true
 
 # Camera - Miui
 TARGET_CAMERA_PACKAGE_NAME := com.android.camera
