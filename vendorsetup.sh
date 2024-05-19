3899568465b353623a416545a52e6aaea061e086
Om Rajesh Doulagar <107272205+omraj12@users.noreply.github.com>
2024-05-19 23:56:05 +0530

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+# Vendor Tree
+git clone https://gitea.com/hdzungx/android_vendor_xiaomi_munch vendor/xiaomi/munch
+
+# Kernel Tree
+git clone https://github.com/EmanuelCN/kernel_xiaomi_sm8250 -b munch kernel/xiaomi/munch
+
+# Leica Camera
+git clone https://gitea.com/hdzungx/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
+
+# Firmware
+git clone https://gitea.com/hdzungx/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
